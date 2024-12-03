d3ad043504aca556a91e532ec389bdaec9cdcf44
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2024-12-03 17:45:52 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (92.17%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+     
+        int index=Arrays.binarySearch(nums, target);
+        if(index>=0){
+            return index;
+        }
+        else{
+            return -(index+1);
+        }
+    }
+}
