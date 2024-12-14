5ae9ede6c6648bced5dce0f5b7876917df87c73b
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2024-12-14 23:26:53 +0530

    Time: 1341 ms (75.31%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -15,18 +15,3 @@ group by
 order by
    v.customer_id;
 
-# SELECT 
-#     v.customer_id,
-#     COUNT(*) AS count_no_trans
-# FROM 
-#     Visits v
-# LEFT JOIN 
-#     Transactions t
-# ON 
-#     v.visit_id = t.visit_id
-# WHERE 
-#     t.transaction_id IS NULL
-# GROUP BY 
-#     v.customer_id
-# ORDER BY 
-#     v.customer_id;
