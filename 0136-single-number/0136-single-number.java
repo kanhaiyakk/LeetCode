9b54e809580318cbbb8a7a57dc2f9772c53b9a31
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2024-12-17 12:53:48 +0530

    Time: 12 ms (21.56%), Space: 44.8 MB (96.18%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,20 +1,31 @@
 class Solution {
     public int singleNumber(int[] nums) {
         
-       // Arrys.sort[nums];
-        for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-            if(nums[i]==nums[j]){ 
-                count++;
+        //using set
+        Set<Integer> set=new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                set.remove(num);
+            }else{
+                set.add(num);
             }
-            }
-            if(count==1){
-                return nums[i];
-            }
-            
         }
+        return set.iterator().next();
         
-        return -1;
+       
+//         for(int i=0;i<nums.length;i++){
+//             int count=0;
+//             for(int j=0;j<nums.length;j++){
+//             if(nums[i]==nums[j]){ 
+//                 count++;
+//             }
+//             }
+//             if(count==1){
+//                 return nums[i];
+//             }
+            
+//         }
+        
+//         return -1;
     }
 }
