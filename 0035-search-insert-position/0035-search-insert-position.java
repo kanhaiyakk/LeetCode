c3df5b099809226ddec4fa6c91dc61151062b14f
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-01-17 15:52:31 +0530

    Time: 0 ms (100%), Space: 43.3 MB (26.8%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,29 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-     
         int low=0;
         int high=nums.length-1;
         while(low<=high){
-        int mid=low+(high-low)/2;
-        if(nums[mid] == target){
-            return mid;
-        }    
-         else if(nums[mid]< target){
-             low=mid+1;
-         }   
-            else{
+            int mid=low+(high-low)/2;
+            if(target==nums[mid]){
+                return mid;
+            }else if(target>nums[mid]){
+                low=mid+1;
+            }else{
                 high=mid-1;
             }
         }
-        return low;
-        
-        
-        // int index=Arrays.binarySearch(nums, target);
-        // if(index>=0){
-        //     return index;
-        // }
-        // else{
-        //     return -(index+1);
-        // }
+          return low;
     }
 }
