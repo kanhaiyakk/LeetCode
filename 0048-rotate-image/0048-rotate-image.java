a803f283e85f818c1ff470e8714660d66cbe15df
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-06-20 20:10:45 +0530

    Time: 0 ms (100%), Space: 42.1 MB (77.09%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -19,11 +19,6 @@ class Solution {
                 k--;
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println();
-        }
+        
     }
 }
