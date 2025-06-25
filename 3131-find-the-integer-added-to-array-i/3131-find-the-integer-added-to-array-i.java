2320de6ecf31443ffb8b8dc8410dfb4282ec04a9
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-06-25 22:48:59 +0530

    Time: 4 ms (2.74%), Space: 43.3 MB (45.28%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
@@ -4,15 +4,9 @@ class Solution {
         Arrays.sort(nums2);
         Set<Integer> set=new HashSet<>();
         for(int i=0;i<nums1.length;i++){
-            if(nums1[i]>nums2[i]){
             int x=nums2[i]-nums1[i];
             set.add(x);
-            }else{
-            int x=nums2[i]-nums1[i];
-            set.add(x);
-            }
         }
-        
         if(set.size()==1){
             for(int num:set){
                 return num;
