c4e7ab4099ebfcd31bfef02df7fc83cb002e659b
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2025-11-11 23:34:13 +0530

    Time: 0 ms (100%), Space: 42.1 MB (7.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=2) return n;
+       if(n<=2) return n;
         int first=1;
         int second=2;
         for(int i=3;i<=n;i++){
@@ -8,6 +8,6 @@ class Solution {
             first=second;
             second=third;
         }
-        return second;
+        return second; 
     }
 }
