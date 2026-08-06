f011ca95164ab1ba83ba09c14cbe235838e39193
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2026-08-06 11:36:31 +0530

    Time: 17 ms (11.56%), Space: 46.4 MB (26.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,13 +3,11 @@ class Solution {
         s=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         int l=0, r=s.length()-1;
         while(l<r){
-            if(s.charAt(l) == (s.charAt(r))){
-                l++;
-                r--;
-            }
-            else{
+            if(s.charAt(l) != (s.charAt(r))){
                 return false;
             }
+            l++;
+            r--;
         }
         return true;
     }
