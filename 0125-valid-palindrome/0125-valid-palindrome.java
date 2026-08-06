03452247e3d744e525ce54008064057132e79054
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2026-08-06 11:20:46 +0530

    Time: 15 ms (19.32%), Space: 47.4 MB (12.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.trim();
-        s=s.replaceAll("[^a-zA-Z0-9]", "");
-        s=s.toLowerCase();
-        StringBuilder sb=new StringBuilder(s);
-        String rev="";
-        rev=sb.reverse().toString();
-        if(rev.equals(s)){
-            return true;
-        }
-        return false;
+        s=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String rev=new StringBuilder(s).reverse().toString();
+        
+        return s.equals(rev);
     }
 }
