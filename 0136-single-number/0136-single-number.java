8719e5c35e9e1d32bcf5f099fbda28f026c20000
Kanhaiya Kumar <88824128+kanhaiyakk@users.noreply.github.com>
2026-08-10 17:40:06 +0530

    Time: 1 ms (99.95%), Space: 47.1 MB (28.4%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,23 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-//Bit Manipulation
-           int  res=0;
-           for(int num: nums){
-            res^=num;
-           }
-           return res;
-
-
-
-        // Map<Integer,Integer> map=new HashMap<>();
-        // for(int num:nums){
-        //     map.put(num,map.getOrDefault(num,0)+1);
-        // }
-        // for(int key: nums){
-        //     if(map.get(key)==1){
-        //         return key;
-        //     }
-        // }
-        // return -1;
+        int xor=0;
+        for(int num:nums){
+            xor^=num;
+        }
+        return xor;
     }
 }
